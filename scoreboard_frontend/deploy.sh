516283e4ca53c3457c74fa066c3882dd7443a4a7
Jacopo <jacopo.corbetta@gmail.com>
2021-04-25 17:40:06 +0200

    fix sed in deploy.sh

--- scoreboard_frontend/deploy.sh
+++ scoreboard_frontend/deploy.sh
@@ -41,6 +41,6 @@ bash -ac ". .env.$BUILD; ./node_modules/.bin/react-scripts build" \
   && rm build/static/*/*.map \
   && rm build/asset-manifest.json \
   && rm build/service-worker.js \
-  && find build/static -regex '.*\.[cj]ss*' -exec sed -i '' '/^\/[/*]# sourceMappingURL/ d' {} \; \
+  && find build/static -regex '.*\.[cj]ss*' -exec sed -i '/^\/[/*]# sourceMappingURL/ d' {} \; \
   && aws --profile ooo s3 cp ./build/index.html s3://$OOO_S3_BUCKET_PREFIX-$BUILD/scoreboard/index.html --cache-control max-age=60 \
   && aws --profile ooo s3 sync $S3DELARG build/ s3://$OOO_S3_BUCKET_PREFIX-$BUILD/scoreboard

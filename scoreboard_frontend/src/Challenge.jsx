18542c04922360ecd163ab154ca5f12d33b6f372
Jacopo <jacopo.corbetta@gmail.com>
2021-04-30 00:31:52 +0200

    Map positions: chosen based on the item_index (opening order) or stable random

--- scoreboard_frontend/src/Challenge.jsx
+++ scoreboard_frontend/src/Challenge.jsx
@@ -6,6 +6,47 @@ import React from "react";
 
 /* XXX: TO REMOVE TAGS: remove them from the tooltip */
 
+
+const PRESET_CHAL_POSITIONS = {
+    0: ["37%","48%"],  // ~= Milan
+    1: ["42%","14%"],  // ~= socal
+}
+
+function rnd_float(seed)
+{
+    /* from v8 -- Robert Jenkins' 32 bit integer hash function.
+     * overkill, but all things I found are either terrible
+     * (like, linear on the seed), or ten pages long */
+    seed = seed & 0xffffffff
+    seed = (seed + 0x7ed55d16 + (seed << 12)) & 0xffffffff
+    seed = (seed ^ 0xc761c23c ^ (seed >>> 19)) & 0xffffffff
+    seed = (seed + 0x165667b1 + (seed << 5)) & 0xffffffff
+    seed = ((seed + 0xd3a2646c) ^ (seed << 9)) & 0xffffffff
+    seed = (seed + 0xfd7046c5 + (seed << 3)) & 0xffffffff
+    seed = (seed ^ 0xb55a4f09 ^ (seed >>> 16)) & 0xffffffff
+    return (seed & 0xfffffff) / 0x10000000;
+}
+function random_map_pct(seed)
+{
+    const f = rnd_float(seed);
+    const i = Math.floor(f*10);
+    return 5 + i*9;
+}
+function get_chal_pos(id, item_index)
+{
+    /* If we have a preference, use it */
+    var preset = PRESET_CHAL_POSITIONS[item_index];
+    if (preset)
+        return preset;
+    /* Otherwise make one up, hoping it won't collide */
+    /* Doesn't work that well, we should try not to use it */
+    return [
+      '' + random_map_pct(item_index) + "%",
+      '' + random_map_pct(item_index+47) + "%"
+    ];
+}
+
+
 function Challenge(props) {
   const { authenticated, id, item_index, points, solved, tags } = props;
 
@@ -17,15 +58,9 @@ function Challenge(props) {
   }
   onClick = () => props.onClick(props);
 
-  let styles = {
-      // TODO: must specify in each challenge or at least make stable, this code runs every time the tab is selected
-      'top': '' + (15 + 9*item_index) + "%",
-      'left': '' + (5 + 14*item_index) + "%",
-  }
-  const styles_chalname = {
-      'top': '' + (15 + 9*item_index) + "%",
-      'left': '' + (5 + 14*item_index) + "%",
-  }
+  const [my_pos_top, my_pos_left] = get_chal_pos(id, item_index);
+  let styles = { 'top': my_pos_top, 'left': my_pos_left };
+  const styles_chalname = { 'top': my_pos_top, 'left': my_pos_left };  /* will overlap, see padding and z-index */
 
   if (solved) {
     classes += " challenge-solved";
